cc60cf17e7c38f7b194e163ad708b8813957d433
shivambirla784 <86475649+shivambirla784@users.noreply.github.com>
2025-09-02 20:30:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
 function convertToRoman(num) {
   	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
+      0:['M',1000],
+	  1:['CM',900],
+      2:['D', 500],
+	  3:['CD',400],
+      4:['C', 100], 
+      5:['L', 50],
+	  6:['XL',40],
+      7:['X', 10],
+	  8:['IX',9],
+      9:['V', 5],
+	  10:['IV',4],
+      11:['I', 1]
     };
 
   //your code here
-
+	let res='';
+	for(let values of Object.values(obj)){
+		let [sym,val] = values;
+		while(num>=val){
+			res+=sym;
+			num-=val;
+		}
+	}
+    return res;
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
